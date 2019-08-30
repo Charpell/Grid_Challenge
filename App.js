f4ef85b70da6a1b740533584f7a52efb71d7606b
charpell <charpellumeh@gmail.com>
2019-08-30 16:59:20 +0100

    finish

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableWithoutFeedback, Animated } from 'react-native';
 
-export default function App() {
+const colors = ['black', 'blue', 'cyan', 'green', 'magenta', 'red', 'yellow'];
+
+const RenderGrid = () => {
+  const [newColor, setNewColor] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.squareContainer}>
+      {colors.map((color,index) => {
+        const nextColor = color === 'yellow' ? 'black' : colors[index + 1];
+        const changeColor = newColor === nextColor;
+        
+        return (
+          <TouchableWithoutFeedback onPress={() => setNewColor(nextColor)} key={index} >
+            <Animated.View style={[styles.square, { backgroundColor: changeColor ? newColor : color }]}/>
+          </TouchableWithoutFeedback>
+        )
+      })}
     </View>
-  );
+  )
 }
 
+
+const App = () => {
+    return (
+      <SafeAreaView style={styles.container}>
+        <RenderGrid />
+        <RenderGrid />
+        <RenderGrid />
+        <RenderGrid />
+        <RenderGrid />
+        <RenderGrid />
+        <RenderGrid />
+      </SafeAreaView>
+    );
+  
+}
+
+export default App
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  squareContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    marginBottom: 5  
+  },
+  square: {
+    flex: 1,
+    aspectRatio: 1
+  }
 });
+
